d7c9e499b6e3a679a122b6381a54d5aa6ab211c7
Ricardo Cruz <ricardo.cb009@gmail.com>
2021-10-05 22:25:38 -0500

    [ADD] Ejemplo

---
+++ emisorTest1.py
@@ -0,0 +1,28 @@
+import sys
+import os
+from firebase import firebase
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# FUNCION PRINCIPAL DEL PROGRAMA
+# try:
+#Variable con la url de la base de adtos
+serverDb = os.getenv('SERVER_FIREBASE_APP')
+#Conexion con las base de datos
+firebaseApp = firebase.FirebaseApplication(serverDb, None)
+
+datos = {'instruction': 'empezando'}
+
+firebaseApp.post('/instructions',datos)
+# while True:
+# Leer DB por metodo GET
+# readData = firebaseApp.get('/', '')
+# values = list(readData.values())
+
+# # Datos leidos
+# print("Datos recibidos dato")
+# print(readData)
+# print(values)
+# except:
+#     sys.exit("Error al conectarse")
