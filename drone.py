1e796e6ed154e1b8bf3e36eeee49af692e482045
Ricardo Cruz <ricardo.cb009@gmail.com>
2021-10-08 16:00:02 -0500

    [UP] Responder mensaje

--- drone.py
+++ drone.py
@@ -15,11 +15,18 @@ class Drone:
         cred = credentials.Certificate("private_key.json")
         firebase_admin.initialize_app(cred, {'databaseURL': self.__serverDb})
         self.__ref = db.reference("/%s%d"%(self.__nameCollection, self.__idDrone))
+        self.setReady()
         self.__ref.listen(self.onInstruction)
+    
+    def setReady(self):
+        self.__ref.parent.delete();
         self.__ref.push({'ins': 'ready'})
+
 
     def onInstruction(self, dataRecived):
         dataRec = dataRecived.data
-        if('ins' in dataRec):
+        if(dataRec != None and 'ins' in dataRec):
             instruction = dataRec['ins']
             print(instruction)
+            if(instruction == 'prepared'):
+                self.setReady()
