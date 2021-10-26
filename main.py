89a3d1125fb4573e4901f7014404bd78a147a8ae
Ricardo Cruz <ricardo.cb009@gmail.com>
2021-10-26 15:22:30 -0500

    [ADD] Agregar script de distancia

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from drone import Drone
+from distanceSensor import DistanceSensor
 
 def main():
-    Drone1 = Drone()
+    # Drone1 = Drone()
+    DistanceSensor1 = DistanceSensor()
+
 
 if __name__ == '__main__':
     main()
