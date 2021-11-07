0df3eca2c19d0e2b2888b5e941290c60bbaa46bb
Ricardo Cruz <ricardo.cb009@gmail.com>
2021-11-07 10:34:15 -0600

    [UP] Sensor ultrasonico

--- distanceSensor.py
+++ distanceSensor.py
@@ -1,3 +1,17 @@
+from signal import signal, SIGTERM, SIGHUP, pause
+from time import sleep
+from gpiozero import DistanceSensor, Buzzer
+
 class DistanceSensor:
     def __init__(self):
-        print("Sensor")
+        try:
+            signal(SIGTERM, safe_exit)
+            signal(SIGHUP, safe_exit)
+
+            sensor.when_in_range = buzzer.on
+            sensor.when_out_of_range = buzzer.off
+
+            pause()
+
+        except KeyboardInterrupt:
+            pass
