cacb9b3c101a3948ffa40d12ec5c0985a64de631
Ricardo Cruz <ricardo.cb009@gmail.com>
2021-11-21 09:06:39 -0600

    [ADD] Uso de hilos

--- distanceSensor.py
+++ distanceSensor.py
@@ -1,20 +1,33 @@
-from time import sleep
 from gpiozero import DistanceSensor 
+from signal import signal, SIGTERM, SIGHUP, pause
+from threading import Thread
+from time import sleep
 
 class Sensor:
     __MAX_DISTANCE = 3.0
+    __isReading = False
     def __init__(self, echo, trigger):
         # Incializar pines
         self.__sensor = DistanceSensor(echo=echo, 
             trigger=trigger, 
             max_distance=self.__MAX_DISTANCE)
+
+    def __readDistance(self):
+        while self.__isReading:
+                distance = self.getDistance
+                print('{:1.2f}'.format(distance) + " cm")
+                sleep(0.5)
     
     def start(self):
         try:
-            while True:
-                distance = self.getDistance
-                print("Distancia: ",'{:1.2f}'.format(distance) + " cm")
-                sleep(0.5)
+            signal(SIGTERM, self.__safe_exit)
+            signal(SIGHUP, self.__safe_exit)
+
+            self.__isReading = True
+            reader = Thread(target=self.__readDistance, daemon=True)
+            reader.start()
+
+            pause() 
         except KeyboardInterrupt:
             pass
         finally:
@@ -24,3 +37,6 @@ class Sensor:
     @property
     def getDistance(self):
         return self.__sensor.distance * 100
+    
+    def __safe_exit(self, signum, frame):
+        exit(1)
