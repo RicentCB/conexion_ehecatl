a1a0f1983fe5ccf8c9794b62a58a635ecf5ad7bd
Ricardo Cruz <ricardo.cb009@gmail.com>
2021-11-22 10:45:20 -0600

    [UP] Test Threads

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 from drone import Drone
 from distanceSensor import Sensor
 
+from threading import Thread
+
+class myClassA(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
+    def run(self):
+        while True:
+            print("A")
+
+class myClassB(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
+    def run(self):
+        while True:
+            print("B")
+
 def main():
     # Drone1 = Drone()
-    Sensor1 = Sensor(20, 21, 'S1')
-    Sensor2 = Sensor(19, 26, 'S2')
-    Sensor1.start()
-    Sensor2.start()
+    # Sensor1 = Sensor(20, 21, 'S1')
+    # Sensor2 = Sensor(19, 26, 'S2')
+    # Sensor1.start()
+    # Sensor2.start()
+    myClassA()
+    myClassB()
+    while True:
+        pass
 
 
 if __name__ == '__main__':
